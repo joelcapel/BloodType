010517306d42e90b827aa12cad753c6b0b723275
joelcapel <jcapel@elpuig.xeill.net>
2022-05-21 20:37:05 +0200

    Mail

--- app/src/main/java/com/company/bda/Adapter/UserAdapter.java
+++ app/src/main/java/com/company/bda/Adapter/UserAdapter.java
@@ -1,6 +1,8 @@
 package com.company.bda.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +13,17 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.company.bda.Email.JavaMailApi;
 import com.company.bda.Model.User;
 import com.company.bda.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -51,6 +62,72 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
         holder.telefono.setText(user.getTelefono());
 
         Glide.with(context).load(user.getProfilepictureurl()).into(holder.imagenPerfilUsuario);
+
+        final String nombreDelReceptor = user.getNombre();
+        final String idDelReceptor = user.getId();
+
+        //enviando email
+
+        holder.emailNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(context)
+                        .setTitle("ENVIAR EMAIL")
+                        .setMessage("Enviar email a " + user.getNombre() + "?")
+                        .setCancelable(false)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                                        .child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                reference.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        String nombreEnvio = snapshot.child("nombre").getValue().toString();
+                                        String email = snapshot.child("email").getValue().toString();
+                                        String telefono = snapshot.child("telefono").getValue().toString();
+                                        String sangre = snapshot.child("gruposangre").getValue().toString();
+
+                                        String mEmail = user.getEmail();
+                                        String mAsunto = "DONACION DE SANGRE";
+                                        String mMensaje = "Hola "+  nombreDelReceptor +", " + nombreEnvio+
+                                                "Quiere recibir una donacion tuya. Aqui estan sus datos: \n"
+                                                +"Nombre: " + nombreEnvio+ "\n"+
+                                                "Numero de telefono: "+ telefono+ "\n"+
+                                                "Email: "+ email +"\n"+
+                                                "Grupo sanguineo: "+ sangre +"\n"+
+                                                "Amablemente comuniquese con él/ella. Gracias!\n"+
+                                                "BLOOD TYPE - DONAR SANGRE, SALVA VIDAS!";
+
+                                        JavaMailApi javaMailApi = new JavaMailApi(context, mEmail, mAsunto, mMensaje);
+                                        javaMailApi.execute();
+
+                                        DatabaseReference envioRef = FirebaseDatabase.getInstance().getReference("emails")
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                        envioRef.child(idDelReceptor).setValue(true).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()){
+                                                    DatabaseReference receptorRef = FirebaseDatabase.getInstance().getReference("emails")
+                                                            .child(idDelReceptor);
+                                                    receptorRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(true);
+                                                }
+                                            }
+                                        });
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/company/bda/Email/Util.java
@@ -0,0 +1,7 @@
+package com.company.bda.Email;
+
+public class Util {
+
+    public static final String EMAIL = "joelcapel97@gmail.com";
+    public static final String PASSWORD = "$$Joelcapel97$$";
+}
